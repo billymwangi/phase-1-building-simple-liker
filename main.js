e8cabbbd2407820df715ad90dbf611e4deb3e42c
billymwangi <billy.mwangi@student.moringaschool.com>
2022-06-30 08:45:33 +0300

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,41 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal= document.querySelector("#modal");
+modal.classList.add("hidden");
 
+
+const heartClick = (event) =>{
+  const heart = event.target
+  heart.innerText = EMPTY_HEART
+  mimicServerCall()
+    .then( res => {
+      if(heart.innerText === EMPTY_HEART){
+        heart.innerText = FULL_HEART
+        heart.classList.add("activated-heart")
+      }
+      else{
+        heart.innerText = EMPTY_HEART
+        heart.classList.remove("activated-heart")
+      }
+      
+      
+    })
+    .catch(error => {
+      modal.classList.remove("hidden");
+      modal.querySelector("#modal-message").textContent = error;
+      setTimeout( () => {
+        modal.classList.add("hidden")
+      }, 3000)
+    }) 
+  
+}
+
+
+const hearts = document.querySelectorAll(".like-glyph");
+hearts.forEach( heart => {
+  heart.addEventListener("click", clickHeart)
+})
 
 
 
